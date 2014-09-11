2516ee5dfc8a4ff8f958810d76f04b5bb7b9c032
Adelino Segundo <adelinosegundo@gmail.com>
2014-09-11 10:40:12 -0300

    functions folder structure upgraded

--- app/templates/theme/application/functions.php
+++ app/templates/theme/application/functions.php
@@ -4,5 +4,4 @@
  */
 require 'functions/configs/config.php';
 require 'functions/configs/content.php';
-require 'functions/utilities/plib_box.php';
-require 'functions/utilities/theme_config.php';
+require 'functions/configs/autoload.php';
---
+++ app/templates/theme/application/functions/configs/autoload.php
@@ -0,0 +1,5 @@
+<?php
+	require 'config.php';
+	require 'utilities/plib_box.php';
+	require 'utilities/theme_config.php';
+	require 'utilities/kresieg_pagination.php';
